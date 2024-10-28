89474e7592f242a73c4a694ae254ee6534039a40
eungaaee <aidjiwon1004@gmail.com>
2024-10-29 01:47:54 +0900

    뜻 보고 영단어 맞추기 게임 구현 (버그 수정 아직 안 함)

--- 20241944_1.c
+++ 20241944_1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
 enum menu { FLASH_CARD = 1, GAME, EXIT };
 
@@ -51,14 +52,50 @@ void shuffle(int *arr, int n) {
     // for (int i = 0; i < 10; i++) printf("%d ", arr[i]);
 }
 
+// get random integer from 0 to n
+static inline int rand_int(int n) {
+    srand(time(NULL));
+    return rand() % (n+1);
+}
+
+static inline void print_wrong_indicator(int wrongCnt) {
+    if (wrongCnt >= 1) printf("                        O \n");
+    else putchar('\n');
+
+    if (wrongCnt >= 4) printf("                      / | \\ \n");
+    else if (wrongCnt >= 3) printf("                      / | \n");
+    else if (wrongCnt >= 2) printf("                        | \n");
+    else putchar('\n');
+
+    if (wrongCnt >= 6) printf("                       / \\ \n");
+    else if (wrongCnt >= 5) printf("                       / \n");
+    else putchar('\n');
+
+    putchar('\n');
+}
+
+int check_char_in_target(const char *target, const int targetLength, const char c, char *const inputField, int *const revealed) {
+    _Bool found = 0;
+
+    for (int i = 0; i < targetLength; i++) {
+        if (target[i] != c) continue;
+        else if (inputField[i] == c) return 1; // if already revealed, return 1
+
+        inputField[i] = c;
+        found = 1;
+        (*revealed)++;
+    }
+
+    return found;
+}
+
 void flash_card() {
     // shuffle order array
     int order[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     shuffle(order, 10);
 
-    // input day, outputMethod
+    // input day and outputMethod
     int day, outputMethod;
-
     printf("학습 일차 : ");
     scanf("%d", &day);
     printf("출력 방식(수동모드 : 1, 자동모드 : 2) : ");
@@ -72,8 +109,8 @@ void flash_card() {
         printf(">> 영어 단어 암기 프로그램 : 플래쉬카드 <<\n\n\n\n\n");
         printf("%s\n", word_dict[day-1][order[i]].eng);
 
-        if (outputMethod == 1) getchar();
-        else if (outputMethod == 2) sleep(1);
+        if (outputMethod == 1) getchar();       // if manual mode, wait for enter key
+        else if (outputMethod == 2) sleep(1);   // if auto mode, wait for 1 second
         system("clear");
 
         printf(">> 영어 단어 암기 프로그램 : 플래쉬카드 <<\n\n\n\n\n");
@@ -85,19 +122,70 @@ void flash_card() {
     }
 }
 
+void game() {
+    // input day
+    int day;
+    printf("학습 일차 : ");
+    scanf("%d", &day);
+    getchar();
+
+    // set random word to target
+    const int targetIndex = rand_int(9);
+    const char *hint = word_dict[day-1][targetIndex].kor;
+    const char *targetWord = word_dict[day-1][targetIndex].eng;
+    const int targetLength = strlen(targetWord);
+
+    // set input field
+    char *const inputField = (char *)malloc(targetLength+1);
+    for (int i = 0; i < targetLength; i++) inputField[i] = '_';
+    inputField[targetLength] = '\0';
+
+    sleep(1);
+
+    // start game
+    int try, wrongCnt = 0, revealed = 0; // wrong count for indicator, revealed for checking if all characters are revealed
+    for (try = 1; wrongCnt < 6 && revealed < targetLength; try++) {
+        system("clear");
+        printf(">> 영어 단어 암기 프로그램 : 게임 <<\n");
+        printf("(힌트) %s\n\n", hint);
+
+        printf("------------------------+\n");
+        print_wrong_indicator(wrongCnt);
+
+        for (int i = 0; i < targetLength; i++) printf("%c ", inputField[i]);
+        printf("%d번째 시도 : ", try);
+
+        char c;
+        c = getchar();
+        getchar();
+        
+        // reveal correct character and check if input is correct
+        if (!check_char_in_target(targetWord, targetLength, c, inputField, &revealed)) wrongCnt++;
+    }
+
+    // game done
+    // TODO: update message one more time
+    if (revealed == targetLength) printf("#####################\n### Congratulations!!! ###\n#####################\n");
+    else printf(">> 실패! 정답은 %s입니다. <<\n", targetWord);
+
+    sleep(2);
+
+    free(inputField);
+}
+
 int main() {
     int select;
 
     do {
-        // show menu and wait for valid select
-        do {
-            printf(">> 영어 단어 암기 프로그램 <<\n");
-            printf("1. 플래쉬카드\t\t2. 게임\t\t3. 프로그램 종료\n\n");
-            printf("번호를 선택하세요 : ");
-            scanf("%d", &select);
+        // show menu and wait for select
+        system("clear");
 
-            system("clear");
-        } while (select < 1 || select > 3);
+        printf(">> 영어 단어 암기 프로그램 <<\n");
+        printf("1. 플래쉬카드\t\t2. 게임\t\t3. 프로그램 종료\n\n");
+        printf("번호를 선택하세요 : ");
+        scanf("%d", &select);
+
+        system("clear");
 
         // do selected stuff
         switch (select) {
@@ -105,10 +193,14 @@ int main() {
                 flash_card();
                 break;
             case GAME:
-                // game();
+                game();
                 break;
             case EXIT:
                 system("clear");
+                break;
+            default: // invalid input
+                printf("입력이 올바르지 않습니다.\n");
+                sleep(1);
                 break;
         }
     } while (select != EXIT);
